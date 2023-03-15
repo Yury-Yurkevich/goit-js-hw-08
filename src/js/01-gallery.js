c12cef45168d47fbce47b30a81182c6d60b542e5
Юрій-Юркевич <yurko0510yurko@gmail.com>
2023-03-15 21:52:28 +0200

    hw-08 js done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const imageElements = [];
 
+
+galleryItems.forEach(imageElement => {
+   const link = document.createElement('a');
+   link.classList.add('gallery__item');
+   link.href = imageElement.original;
+
+   const image = document.createElement('img');
+   image.classList.add('gallery__image');
+   image.src = imageElement.preview;
+//    image.dataset.source = imageElement.original;
+   image.alt = imageElement.description;
+
+   link.appendChild(image);
+
+   imageElements.push(link);
+});
+
+galleryContainer.append(...imageElements);
+
+let lightbox = new SimpleLightbox('.gallery a', { caption: true, captionSelector: 'img[alt]', captionType: 'attr', captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 }); 
+
+ 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const formTextArea = document.querySelector(`.feedback-form textarea`);
+const form = document.querySelector(`form`);
+const emailLabel = document.querySelector(`.feedback-form input`);
+const STORAGE_KEY = `feedback-form-state`;
+let formData = {};
+
+const saveData = evt => {
+  const value = evt.target.value;
+  const name = evt.target.name;
+  formData[name] = value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+//
+const submitForm = evt => {
+  evt.preventDefault();
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+  if (email.value === '' || message.value === '') {
+    return alert(`Please fill all fields`);
+  }
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  evt.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
+};
+//
+const populateTextArea = evt => {
+  const parcedMessageToSave = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (parcedMessageToSave) {
+    formTextArea.value = parcedMessageToSave.message;
+    emailLabel.value = parcedMessageToSave.email;
+  }
+};
+//
+populateTextArea();
+//
+form.addEventListener(`submit`, submitForm);
+form.addEventListener(`input`, throttle(saveData, 500));
